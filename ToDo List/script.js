a9d57aa71a88bf4fa5fe605e51e5ca6544525856
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-06-13 22:34:36 +0530

    Update script.js

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -1,33 +1,58 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function ajax(){
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function(){
-    if (this.readyState ==4 && this.status ==200){
-        document.getElementById("demo").innerHTML=this.responseText;
+function validate(){
+let uname = document.getElementById("uname").value;
+let pwd = document.getElementById("pwd").value;
+let error1 = document.getElementById("error1");
+let error2 = document.getElementById("error2");
+  if(!(uname == "admin")){
+      document.getElementById("error1").innerHTML="Invalid Id";
+      error1.style.color="red";
+  } 
+     
+    else if(!(pwd == 12345)){
+       document.getElementById("error2").innerHTML="invalid";
+        error2.style.color="red";
+       return false;
     }
-};
-xhttp.open("GET","ajax_info.txt",true);
-xhttp.send();
+
+    else{
+         validate1(validate2);
+    }
 }
+    function validate1(callback){
+            let uname = document.getElementById("uname").value;
+             let pwd = document.getElementById("pwd").value;   
+            
+                 if(uname == "admin" && pwd == 12345){
+                     callback();
+                    // alert(validate1);
+                 }
+                 else{
+                     alert("Wrong");
+                 }
+                }             
+        function validate2(){
+         window.location.href = "todo.html";  
+        }
+              
+        
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
