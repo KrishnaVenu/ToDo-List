2debb7fe4e70672a9eada905d9627580ce005a0d
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-08-02 18:52:54 +0530

    Update script.js

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -1,51 +1,29 @@
-function validate(){
-let uname = document.getElementById("uname").value;
-let pwd = document.getElementById("pwd").value;
-let error1 = document.getElementById("error1");
-let error2 = document.getElementById("error2");
-  if(!(uname == "admin")){
-      document.getElementById("error1").innerHTML="Invalid Id";
-      error1.style.color="red";
+
+function validate(callback){
+    var  uname = document.getElementById("uname").value;
+    var pwd = document.getElementById("pwd").value;
+    //alert(9);
+  // callback()
+  if(uname == "admin" && pwd == "12345"){
+      //console.log(9);
+      callback();
+      alert("Login Successfully");
+      return true;
   } 
      
-    else if(!(pwd == 12345)){
-       document.getElementById("error2").innerHTML="invalid";
-        error2.style.color="red";
+    else if(uname == "" || pwd == ""){
+   alert("Input fields cannot be empty");
        return false;
     }
-
     else{
-         validate1(validate2);
-    }
-}
-    function validate1(callback){
-            let uname = document.getElementById("uname").value;
-             let pwd = document.getElementById("pwd").value;   
-            
-                 if(uname == "admin" && pwd == 12345){
-                     callback();
-                    // alert(validate1);
-                 }
-                 else{
-                     alert("Wrong");
-                 }
-                }             
-        function validate2(){
-         window.location.href = "todo.html";  
+         alert("Wrong");
+         return false;
         }
-              
-        
-                
-
-
-
-
-
-
-
-
-
-
+    }           
+function validate2(){
+        window.location.href = "todo.html";  
+     }
+       
 
 
 
